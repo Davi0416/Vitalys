86c9594a446a109fde97c9508487c6a8ed7b5619
Davi Asafe <daviasafe8@gmail.com>
2026-04-19 18:37:30 -0300

    feat: arquivos iniciais backend

---
+++ vitalys-backend/src/main/java/com/vitalys/backend/VitalysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vitalys.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalysBackendApplication.class, args);
+	}
+
+}
