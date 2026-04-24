24ad7e42efef2ac6e48febe26d328d014fbdebaf
Davi Asafe <daviasafe8@gmail.com>
2026-04-23 21:41:54 -0300

    feat: add usuarios endpoint

---
+++ vitalys-backend/src/main/java/com/vitalys/backend/repository/UsuariosRepository.java
@@ -0,0 +1,7 @@
+package com.vitalys.backend.repository;
+
+import com.vitalys.backend.model.Usuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+}
