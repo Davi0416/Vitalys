ccc193e6e8f9296a9db5e4b7399f83a27377a8ce
Davi Asafe <daviasafe8@gmail.com>
2026-04-25 18:30:50 -0300

    feat: add cors config and dto classes

--- vitalys-backend/src/main/java/com/vitalys/backend/controller/ProfissionalController.java
+++ vitalys-backend/src/main/java/com/vitalys/backend/controller/ProfissionalController.java
@@ -1,8 +1,10 @@
 package com.vitalys.backend.controller;
 
 
+import com.vitalys.backend.dto.RegistrarProfissionalDTO;
 import com.vitalys.backend.model.Profissional;
 import com.vitalys.backend.repository.ProfissionalRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +16,25 @@ public class ProfissionalController {
     private ProfissionalRepository profissionalRepository;
 
     @PostMapping(path="/profissionais")
-    public Profissional create(@RequestBody Profissional profissional){
-        return profissionalRepository.save(profissional);
+    public Profissional create(@RequestBody RegistrarProfissionalDTO profissional){
+        Profissional newProfissional = new Profissional().registrar(profissional);
+        return profissionalRepository.save(newProfissional);
     }
 
     @GetMapping(path = "/profissionais")
     public @ResponseBody Iterable<Profissional> findAll(){
         return profissionalRepository.findAll();
+    }
+
+    @DeleteMapping(path = "/profissinais/{id}")
+    public void deleteProfissional(@PathVariable Long id) {
+       profissionalRepository.deleteById(id);
+    }
+
+    @PutMapping(path = "/profissionais/{id}")
+    public Profissional updateProfissional(@PathVariable Long id, @RequestBody @Valid RegistrarProfissionalDTO profissional) {
+        Profissional profissionalExistente = profissionalRepository.findById(id).orElseThrow(() -> new RuntimeException("Profissional não encontrado"));
+        profissionalExistente.atualizar(profissional);
+        return profissionalRepository.save(profissionalExistente);
     }
 }
--- vitalys-backend/src/main/java/com/vitalys/backend/model/Paciente.java
+++ vitalys-backend/src/main/java/com/vitalys/backend/model/Paciente.java
@@ -1,5 +1,6 @@
 package com.vitalys.backend.model;
 
+import com.vitalys.backend.dto.RegistrarPacienteDTO;
 import jakarta.persistence.*;
 
 import java.util.Date;
@@ -7,6 +8,8 @@ import java.util.Date;
 @Entity
 @Table(name = "paciente")
 public class Paciente {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +31,39 @@ public class Paciente {
 
     @Column(name = "telefone")
     private String telefone;
+
+    public Paciente(String nome, String cpf, String email,  Date dataNascimento, String endereco, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Paciente registrar(RegistrarPacienteDTO dto) {
+        this.nome = dto.getNome();
+        this.cpf = dto.getCpf();
+        this.email = dto.getEmail();
+        this.dataNascimento = dto.getDataNascimento();
+        this.endereco = dto.getEndereco();
+        this.telefone = dto.getTelefone();
+        return this;
+    }
+
+
+    public void atualizar(RegistrarPacienteDTO paciente) {
+        this.nome = paciente.getNome();
+        this.cpf = paciente.getCpf();
+        this.email = paciente.getEmail();
+        this.dataNascimento = paciente.getDataNascimento();
+        this.endereco = paciente.getEndereco();
+        this.telefone = paciente.getTelefone();
+    }
+
+
+    public Paciente() {
+    }
 
     public Long getId() {
         return id;
@@ -84,4 +120,5 @@ public class Paciente {
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
+
 }
