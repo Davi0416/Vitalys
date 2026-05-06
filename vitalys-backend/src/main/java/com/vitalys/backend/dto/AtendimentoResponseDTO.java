435c01ed5e88833547a90cb64a733a6ed2fa4e4f
Davi Asafe <daviasafe8@gmail.com>
2026-05-05 21:27:42 -0300

    fix: corrigido bugs do calendario

--- vitalys-backend/src/main/java/com/vitalys/backend/dto/AtendimentoResponseDTO.java
+++ vitalys-backend/src/main/java/com/vitalys/backend/dto/AtendimentoResponseDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public record AtendimentoResponseDTO(
         Long id,
+        Long idProfissional,
         String nomePaciente,
         String nomeProfissional,
         LocalDateTime dataEHoraMarcadas) {
